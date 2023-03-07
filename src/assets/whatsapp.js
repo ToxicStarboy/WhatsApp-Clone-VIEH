833427064a17df77ed1a7c08db6981ff91a49e10
Aadesh Gupta <aadeshgupta2003@gmail.com>
2023-03-07 15:10:11 +0530

    first commit

---
+++ src/assets/whatsapp.js
@@ -0,0 +1,37 @@
+import pp from "./images/pp.png";
+import cs1 from "./images/cs1.png";
+import cs2 from "./images/cs2.jpg";
+import brook from "./images/brook.jpg";
+import aditya from "./images/aditya.jpeg";
+import meme from "./images/meme.png";
+import nopp from "./images/nopp.png";
+import harsh from "./images/harsh.jpeg";
+import aman from "./images/aman.jpeg";
+import trump from "./images/trump.jfif";
+import boss from "./images/boss.png";
+import rahul from "./images/rahul.jpg";
+import burgerking from "./images/burgerking.png";
+import doggo from "./images/doggo.jfif";
+import acdc from "./images/acdc.jfif";
+import kevin from "./images/kevin.jpeg";
+import zucc from "./images/zucc.jpeg";
+
+export {
+  pp,
+  cs1,
+  cs2,
+  brook,
+  aditya,
+  meme,
+  nopp,
+  harsh,
+  aman,
+  trump,
+  boss,
+  rahul,
+  burgerking,
+  doggo,
+  acdc,
+  kevin,
+  zucc
+};
